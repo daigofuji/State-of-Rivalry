a875ade3805bd57921f1209a601d310a980e5a2f
Tom Giratikanon <giratikanon@gmail.com>
2012-03-24 16:26:13 -0400

    Made the filtering work!

--- www/js/libs/dropdown.js
+++ www/js/libs/dropdown.js
@@ -1,50 +1,55 @@
 $(document).ready(function() {
-    createDropDown();
-    
-    $(".dropdown dt a").click(function() {
-        $(".dropdown dd ul").toggle();
-    });
-
-    $(document).bind('click', function(e) {
-        var $clicked = $(e.target);
-        if (! $clicked.parents().hasClass("drop down"))
-            $(".dropdown dd ul").hide();
-    });
-                
-    $(".dropdown dd ul li a").click(function() {
-        var text = $(this).html();
-        $(".dropdown dt a").html(text);
-        $(".dropdown dd ul").hide();
-        
-        var source = $("#source");
-        source.val($(this).find("span.value").html())
-    });
-    
+    /*
     $("select").each(function() {
     	
-    	createDropDown($(this))
+    	var menu = createDropDown($(this))
+    	
+    	menu.find(".dropdown dt a").click(function() {
+	        menu.find(".dropdown dd ul").toggle();
+	    });
+	
+	    $(document).bind('click', function(e) {
+	        var $clicked = $(e.target);
+	        if (! $clicked.parents().hasClass("drop down"))
+	            $(".dropdown dd ul").hide();
+	    });
+	                
+	    menu.find(".dropdown dd ul li a").click(function() {
+	        var text = $(this).html();
+	        menu.find(".dropdown dt a").html(text);
+	        menu.find(".dropdown dd ul").hide();
+	        
+	        var source = $("#source");
+	        // source.val($(this).find("span.value").html())
+	    });
     
-    });
+    });*/
+    
+    
 });
 
-function onDropDownChange() {
-
-
-}
-
 function createDropDown(source){
-    var selected = source.find("option[selected]");
+
+    var selected = source.find("option:selected");
     var options = $("option", source);
     
-    $("body").append('<dl id="target" class="drop down"></dl>')
-    $("#target").append('<dt><a href="#">' + selected.text() + 
+    var menu = $('<dl class="drop down"></dl>');
+    
+    menu.append('<dt><a href="#">' + selected.text() + 
         '<span class="value">' + selected.val() + 
         '</span></a></dt>')
-    $("#target").append('<dd><ul></ul></dd>')
+    menu.append('<dd><ul></ul></dd>')
 
     options.each(function(){
-        $("#target dd ul").append('<li><a href="#">' + 
+        menu.find("dd ul").append('<li><a href="#">' + 
             $(this).text() + '<span class="value">' + 
             $(this).val() + '</span></a></li>');
     });
+    
+    console.log(menu);
+    
+    source.replaceWith(menu);
+    
+    return menu;
+    
 }
--- www/js/query.js
+++ www/js/query.js
@@ -30,69 +30,95 @@ rivalry.MatchupExplorer = Backbone.View.extend({
 	
 	
 	events: {
-		"change input": "update"
+		"change select": "render"
 	},
 
 	initialize: function() {
 		
 		_.bindAll(this);
-		this.model.on("change", this.render)
+		// this.model.on("change", this.render)
 		
 		// $.get("", this.onData);
 		
-		this.onData({
-			1: { "winner": "Yankees", shutout: true, time: "day" },
-			2: { "winner": "Yankees", shutout: true, time: "night" },
-			3: { "winner": "Yankees", shutout: false, time: "day" },
-			4: { "winner": "Red Sox", shutout: false, time: "day" },
-			5: { "winner": "Red Sox", shutout: false, time: "night" },
-			6: { "winner": "Red Sox", shutout: true, time: "night" }
-		})
+		this.onData([{"id":10361,"name":"Tampa Bay Rays-Baltimore Orioles","started_at":"2012-03-05T13:05:00-05:00","home_team":{"id":451,"name":"Tampa Bay Rays","abbrev":"TAM","nickname":"Rays","location":"Tampa Bay","hashtag":"Rays","sports_ml_key":"l.mlb.com-t.4","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}},"home_team_score":1,"home_team_outcome":"loss","away_team":{"id":448,"name":"Baltimore Orioles","abbrev":"BAL","nickname":"Orioles","location":"Baltimore","hashtag":"Orioles","sports_ml_key":"l.mlb.com-t.1","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}},"away_team_score":3,"away_team_outcome":"win","description":null,"upcoming":false,"in_progress":false,"ended":true,"scored":false,"sports_ml_key":"l.mlb.com-2012-e.35727","game_stat":{"id":134,"winning_pitcher":{"id":8818,"name":"Brad Bergesen","position":"U","sports_ml_key":"l.mlb.com-p.18028","team":{"id":448,"name":"Baltimore Orioles","abbrev":"BAL","nickname":"Orioles","location":"Baltimore","hashtag":"Orioles","sports_ml_key":"l.mlb.com-t.1","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}}},"losing_pitcher":{"id":8830,"name":"Alex Cobb","position":"U","sports_ml_key":"l.mlb.com-p.21254","team":{"id":451,"name":"Tampa Bay Rays","abbrev":"TAM","nickname":"Rays","location":"Tampa Bay","hashtag":"Rays","sports_ml_key":"l.mlb.com-t.4","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}}},"saving_pitcher":{"id":8821,"name":"Pedro Strop","position":"U","sports_ml_key":"l.mlb.com-p.15147","team":{"id":448,"name":"Baltimore Orioles","abbrev":"BAL","nickname":"Orioles","location":"Baltimore","hashtag":"Orioles","sports_ml_key":"l.mlb.com-t.1","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}}},"attendance":"3720","duration":"163"}},{"id":10444,"name":"St. Louis Cardinals-Miami Marlins","started_at":"2012-03-05T13:05:00-05:00","home_team":{"id":463,"name":"St. Louis Cardinals","abbrev":"STL","nickname":"Cardinals","location":"St. Louis","hashtag":"Cardinals","sports_ml_key":"l.mlb.com-t.25","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}},"home_team_score":3,"home_team_outcome":"loss","away_team":{"id":465,"name":"Miami Marlins","abbrev":"MIA","nickname":"Marlins","location":"Miami","hashtag":"Marlins","sports_ml_key":"l.mlb.com-t.16","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}},"away_team_score":4,"away_team_outcome":"win","description":null,"upcoming":false,"in_progress":false,"ended":true,"scored":false,"sports_ml_key":"l.mlb.com-2012-e.35836","game_stat":{"id":133,"winning_pitcher":{"id":8846,"name":"Josh Johnson","position":"U","sports_ml_key":"l.mlb.com-p.11066","team":{"id":465,"name":"Miami Marlins","abbrev":"MIA","nickname":"Marlins","location":"Miami","hashtag":"Marlins","sports_ml_key":"l.mlb.com-t.16","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}}},"losing_pitcher":{"id":8861,"name":"Kyle Lohse","position":"U","sports_ml_key":"l.mlb.com-p.5900","team":{"id":463,"name":"St. Louis Cardinals","abbrev":"STL","nickname":"Cardinals","location":"St. Louis","hashtag":"Cardinals","sports_ml_key":"l.mlb.com-t.25","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}}},"saving_pitcher":{"id":8852,"name":"Dan Jennings","position":"U","sports_ml_key":"l.mlb.com-p.20722","team":{"id":465,"name":"Miami Marlins","abbrev":"MIA","nickname":"Marlins","location":"Miami","hashtag":"Marlins","sports_ml_key":"l.mlb.com-t.16","league":{"id":5,"name":"Major League Baseball","abbrev":"MLB","sports_ml_key":"l.mlb.com"}}},"attendance":"6235","duration":"158"}}]
+)
 		
+		
+		this.render();
 	},
 	
 	onData: function(data) {
 		
+		data = _.reduce(data, function(memo, item, index) {
+			item.home_team_abbrev = item.home_team.abbrev;
+			item.score_margin = Math.abs(item.home_team_score - item.away_team_score);
+			
+			var home_outcomes = {
+				"loss": item.away_team,
+				"win": item.home_team,
+				"tie": {
+					abbrev: "TIE"
+				}
+			}
+			
+			item.winner = home_outcomes[item.home_team_outcome];
+			item.winner_abbrev = item.winner.abbrev;
+			memo[index] = item;
+			return memo;
+		}, {});
 		
 		var game_data = {
 			properties: {
-				"winner": { "type": "string", "unique": true },
-				"shutout": { "type": "string", "unique": true },
-				"time": { "type": "string", "unique": true }		
+				"home_team_abbrev": { "type": "string", unique: true },
+				"winner_abbrev": { "type": "string", unique: true },
+				"day_or_night": { "type": "string", unique: true },
+				"score_margin": { "type": "number", unique: true },
 			},
 			items: data
 		};
+		
+		console.log("data", data);
 		
 		this.model.set("games", new Data.Collection(game_data));
 		
 	},
 	
 	render: function() {
-	
-		console.log(this.summarize({}));
 		
-		_.fill(this.summarize({}));
+		this.update();
+		
+		var conditions = this.model.get("conditions");
+		
+		console.log("!", conditions);
+	
+		console.log(this.summarize(conditions));
+		
+		_.fill(this.summarize(conditions));
 		
 	},
 	
 	update: function() {
 		
-		this.model.set("conditions", _.reduce($("form").serializeArray(), function(memo, value, key) {
+		var conditions = _.reduce($("form").serializeArray(), function(memo, record) {
+			value = record.value;
+			key = record.name;
+			if (value == "-") return memo;
+			if (key == "score_margin" && value > 1) key = "score_margin>=";
 			memo[key] = value;
 			return memo;
-		}, {}));
+		}, {});
+		
+		this.model.set("conditions", conditions);
+		
+		return conditions;
 		
 	},
 	
 	summarize: function(conditions) {
 		
-		/* this.model.get("").filter().group(["official_language"], {
-		  "population": {aggregator: Data.Aggregators.COUNT }
-		}); */
-		
 		return {
-			".yankees": this.model.count(_.safe_extend(conditions, { "winner": "Yankees" })),
-			".redsox": this.model.count(_.safe_extend(conditions, { "winner": "Red Sox" })),
+			".num.ny": this.model.count(_.safe_extend(conditions, { "winner_abbrev": "BAL" })),
+			".num.bos": this.model.count(_.safe_extend(conditions, { "winner_abbrev": "TAM" })),
 			".total": this.model.count(_.safe_extend(conditions))
 		};
 		
@@ -128,7 +154,8 @@ _.mixin({
 	$(function() {
 		
 		rivalry.explorer = new rivalry.MatchupExplorer({ 
-			model: new rivalry.Matchup()	
+			model: new rivalry.Matchup(),
+			el: $("form")	
 		});
 	
 	});
